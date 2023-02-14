e7089c26566e0df16a6583634bafe3f540ca0705
haru-blank <avash.nekros274@gmail.com>
2023-02-14 23:00:03 +0545

    feat: add tools icon on ui

--- src/Components/Devtools.tsx
+++ src/Components/Devtools.tsx
@@ -1,8 +1,24 @@
-import { Box } from "@mantine/core";
+import { Box, createStyles, ThemeIcon } from "@mantine/core";
 import { UseFormReturnType } from "@mantine/form/lib/types";
+import ToolsIcon from "./ToolsIcon";
+
+const useStyles = createStyles((theme) => ({
+  toolsIcon: {
+    position: "fixed",
+    top: theme.spacing.md,
+    right: theme.spacing.md,
+  },
+}));
 
 const DevTools = ({ form }: any) => {
-  return <Box>Dev Tools</Box>;
+  const { classes } = useStyles();
+  return (
+    <Box>
+      <ThemeIcon className={classes.toolsIcon}>
+        <ToolsIcon />
+      </ThemeIcon>
+    </Box>
+  );
 };
 
 export default DevTools;
