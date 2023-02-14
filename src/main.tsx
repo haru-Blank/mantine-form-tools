48a8307731f574fc03c94f0da5e4a72059f1d237
haru-blank <avash.nekros274@gmail.com>
2023-02-14 22:10:51 +0545

    feat: wrap app with mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const AppConfig = () => (
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <App />
+  </MantineProvider>
+);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppConfig />
   </React.StrictMode>
 );
