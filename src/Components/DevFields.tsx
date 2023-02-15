c1f5a95d485061303a747e868fb8c8a67e1a600b
haru-blank <avash.nekros274@gmail.com>
2023-02-15 15:13:21 +0545

    feat: dev fields style and functionality

--- src/Components/DevFields.tsx
+++ src/Components/DevFields.tsx
@@ -1,35 +1,73 @@
-import { Accordion, Text } from "@mantine/core";
+import { Accordion, Box, createStyles, Text } from "@mantine/core";
 import React from "react";
 
 type TType = {
   children: React.ReactNode;
 };
+
 const Label = ({ children }: TType) => (
-  <Text mr={"sm"} fw={"bold"} component="span">
-    {children}
+  <Text mr={"sm"} fw={"bold"} component="span" tt={"capitalize"}>
+    {children}:
   </Text>
 );
 
-const Value = ({ children }: TType) => {
-  return <Text>{children}</Text>;
+const Value = ({ value }: any) => {
+  const valueType = typeof value;
+  let valueToDisplay;
+  let valueColor;
+
+  if (valueType === "boolean") {
+    valueToDisplay = value ? "True" : "False";
+    valueColor = value ? "green" : "red";
+  } else {
+    valueToDisplay = value;
+  }
+  return (
+    <Text component="span" color={valueColor}>
+      {valueToDisplay}
+    </Text>
+  );
 };
 
-const DevFields = ({ form, fields }: any) => {
+const useStyles = createStyles((theme) => ({
+  control: {
+    "&[data-active=true]": {
+      background: theme.colors.cyan[2],
+    },
+  },
+}));
+
+const DevFields = ({ form }: any) => {
+  const fields = Object.keys(form.values);
+  const { classes } = useStyles();
+
   return (
     <Accordion variant="contained" multiple>
       {fields.map((field: any) => {
-        const value = form.values[field];
-        const dirty = form.isDirty(field);
-        const touched = form.isTouched(field);
-        const isValid = form.isValid(field);
+        const data = {
+          value: form.values[field],
+          dirty: form.isDirty(field),
+          touched: form.isTouched(field),
+          isValid: form.isValid(field),
+        };
+
+        const fieldsToDisplay = Object.keys(data);
 
         return (
           <Accordion.Item key={field} value={field}>
-            <Accordion.Control>Name: {field}</Accordion.Control>
+            <Accordion.Control className={classes.control}>
+              {field}
+            </Accordion.Control>
             <Accordion.Panel>
               <>
-                <Label>Value</Label>
-                <Value>{value}</Value>
+                {fieldsToDisplay.map((field) => {
+                  return (
+                    <Box key={field + "xyz"}>
+                      <Label>{field}</Label>
+                      <Value value={data[field]} />
+                    </Box>
+                  );
+                })}
               </>
             </Accordion.Panel>
           </Accordion.Item>
